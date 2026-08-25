27b26dcfa5fd7ea83d2d97e8b5edaa44f8e09a9a
jozzydgr8 <jozzydgreat1@gmail.com>
2026-08-25 09:04:24 -0700

    added multer route to gallery

--- routes/galleryRoute.js
+++ routes/galleryRoute.js
@@ -1,8 +1,10 @@
 const {getGallery, createGallery, deleteGallery} = require('../controller/galleryController');
 const Authenticator = require('../middleware/authenticator');
 const router = require('express').Router();
+const multerUpload = require('../config/multerConfig');
+
 router.get('/', getGallery);
-router.post('/', Authenticator, createGallery);
+router.post('/', Authenticator,  multerUpload.single('image'), createGallery);
 router.delete('/:id', Authenticator, deleteGallery);
 
 module.exports = router
